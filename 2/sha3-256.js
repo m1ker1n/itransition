f24f83cbe04f9544456368d285cd6b74f795c16d
M1ker1n <m1ker1nigor@yandex.ru>
2022-04-03 01:19:16 +0300

    2

---
+++ 2/sha3-256.js
@@ -0,0 +1,26 @@
+var sha3_256 = require('js-sha3').sha3_256;
+var fs = require('fs');
+
+var result = [];
+
+var temp = fs.readdirSync('data','utf-8');
+temp.forEach( (file) => {
+    result.push(sha3_256(fs.readFileSync('data/'+file, 'binary')));
+});
+
+result.sort();
+
+var str = '';
+result.forEach( (hash) => {
+    str += hash;
+});
+
+var email = 'm1ker1nigor@yandex.ru';
+str += email;
+str = sha3_256(str);
+console.log(str);
+
+/*
+> var fs = require('fs'); var sha3_256=require('js-sha3').sha3_256; console.log(sha3_256(fs.readFileSync('data/file_00.data','binary')));
+79baf42517b5518db4f36a896b98acf68949fed55f1b5533f1018b48d966f674
+*/
